9e2495309f39bbdbab4b9a9022c51e2247be4805
Remi Gastaldi <remi.gastaldi@epitech.eu>
2019-07-14 20:39:38 +0200

    Add base code

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+/*
+ * Copyright - 2019 GASTALDI Rémi
+ * Licensed under the GPLv2 License. See License.txt in the project root for license information.
+ */
+
+int main(int ac, char *av[]) {
+  (void)ac;
+  (void)av;
+  return 0;
+}
